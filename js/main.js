459094145305a7824b5760ce86ae8fd4b3ffd45d
Jorge Ignacio Lara Ceballos <zero19b@hotmail.com>
2021-01-21 13:27:43 -0300

    primer commit del projecto schauff

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(window).scroll(function (){
+    const scrl = $(window).scrollTop();
+    if(scrl < 60)
+        {
+            $('.header-1').removeClass('fixedbar');
+        }
+    else{
+        $('.header-1').addClass('fixedbar');
+    }
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName("myslides");
+    if(n > slides.length){
+        slideIndex = 1
+    }
+    
+    if(n < 1){slideIndex = slides.length}
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+    
+    
+}
